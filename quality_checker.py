c1045c58101567387e41a040a59b6940c7ce6131
magarharshal452-lang <magarharshal452@gmail.com>
2026-06-13 17:17:52 +0530

    Create quality_checker.py

---
+++ quality_checker.py
@@ -0,0 +1,21 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)
+
+def similarity_score(
+    original,
+    rewritten
+):
+
+    emb1 = model.encode([original])
+    emb2 = model.encode([rewritten])
+
+    score = cosine_similarity(
+        emb1,
+        emb2
+    )[0][0]
+
+    return round(score * 100, 2)
